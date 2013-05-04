8c381ce3aac475b2e4af9fcfe52a55b63472818b
Robert Konrad <robert@ktx-software.com>
2013-05-04 11:52:02 +0200

    Removed unused varyings in level shaders (breaks when compiling to HLSL).

--- Sources/Shaders/level.frag.glsl
+++ Sources/Shaders/level.frag.glsl
@@ -5,7 +5,6 @@ precision highp float;
 #endif
 
 uniform sampler2D sampler;
-varying vec4 position;
 varying vec2 texcoord;
 
 void kmain() {
--- Sources/Shaders/level.vert.glsl
+++ Sources/Shaders/level.vert.glsl
@@ -2,10 +2,9 @@
 
 attribute vec3 pos;
 attribute vec2 tex;
-varying vec4 position;
 varying vec2 texcoord;
 
 void kmain() {
 	texcoord = tex;
-	gl_Position = position = vec4(pos.x, pos.y, pos.z * pos.z, pos.z);
+	gl_Position = vec4(pos.x, pos.y, pos.z * pos.z, pos.z);
 }
