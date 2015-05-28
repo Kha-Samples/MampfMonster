006fe96ad9c7f93e8c8be2689bbd4f8e0cda4dea
Robert Konrad <robert@ktx-software.com>
2015-05-28 04:18:23 +0200

    Updated Kha, fixed compilation and moved assets.

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -4,7 +4,7 @@ import kha.Starter;
 
 class Main {
 	public static function main() {
-		kha.Sys.needs3d = true;
+		//kha.Sys.needs3d = true;
 		var starter = new Starter();
 		starter.start(new YolkfolkRestaurant2());
 		//starter.start(new YolkfolkRestaurant());
